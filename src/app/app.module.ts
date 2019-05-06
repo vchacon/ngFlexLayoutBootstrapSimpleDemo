57b59d1bdf0b56f6d50107f57dd9b51fa38daf06
Victor Chacon <victorchacon@Victors-MBP.fios-router.home>
2019-05-05 23:41:13 -0700

    ÏÏ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ToolbarModule } from 'primeng/toolbar';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
